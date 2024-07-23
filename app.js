ca02a256a360a5c25db90e7186463517119acafe
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2024-07-22 23:37:06 -0300

    se cambio las claves de encriptacion a un map y se agrega la funcion de encriptar

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const minusculasRegEx = /^[a-z\s]+$/;
-const vocales = ["a", "e", "i", "o", "u"];
-let llavesDeEncriptacion = {
-    a: "ai",
-    e: "enter",
-    i: "imes",
-    o: "ober",
-    u: "ufat" 
-};
+
+const llavesDeEncriptacion = new Map([
+    ['a', 'ai'],
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['o', 'ober'],
+    ['u', 'ufat'],
+])
 
 console.log(llavesDeEncriptacion["a"])
 
@@ -15,7 +15,8 @@ function validarTexto(texto){
     return minusculasRegEx.test(texto);
 }
 
-function encriptar(){
+function encriptar(texto){
+
     // Recibir el texto del input
     let texto = document.getElementById('texto').value;
     let textoCifrado = "";
@@ -23,15 +24,12 @@ function encriptar(){
     if(validarTexto(texto)){
         // Se tiene que verificar cada caracater de la cadena de texto
         for(let i = 0; i < texto.length; i++){
-            if(vocales.includes(texto[i])){
-                textoCifrado += llavesDeEncriptacion[texto[i]];
+            if(llavesDeEncriptacion.has(texto[i])){
+                textoCifrado += llavesDeEncriptacion.get(texto[i]);
             }else{
                 textoCifrado += texto[i];
             }
         }
-        // En caso de que sean consonates no modifican su valor final.
-        // En caso de que sean vocales se debe reemplazar con la llave de encriptación correspondiente.
-        // Una vez recorrido todo el texto se debe devolver con la encriptación correspondiente.
     }else{ // Si no está en minúscula o contiene números y caracteres especiales lanzar una alerta
         alert("El texto debe contener solo letras minúsculas")
         return
@@ -39,3 +37,47 @@ function encriptar(){
     console.log(textoCifrado)
  
 }
+
+function desencriptar(){
+    let texto = document.getElementById('texto').value;
+    let textoCifrado = texto;
+    for (const [key, value] of llavesDeEncriptacion.entries()) {
+        if(textoCifrado.includes(value)){
+            textoCifrado = textoCifrado.replaceAll(value, key)    
+        }
+    }
+    console.log(textoCifrado)
+}
+
+
+for (const [key, value] of llavesDeEncriptacion.entries()) {
+    console.log(`${key}: ${value}`);
+  }
+
+// En caso de que sean consonates no modifican su valor final.
+// En caso de que sean vocales se debe reemplazar con la llave de encriptación correspondiente.
+// Una vez recorrido todo el texto se debe devolver con la encriptación correspondiente.
+
+// const vocales = ["a", "e", "i", "o", "u"];
+// let llavesDeEncriptacion = {
+//     a: "ai",
+//     e: "enter",
+//     i: "imes",
+//     o: "ober",
+//     u: "ufat" 
+// };
+
+  
+//     textoCifrado = texto
+//         .replace(/enter/gi, "e")
+//         .replace(/imes/gi, "i")
+//         .replace(/ai/gi, "a")
+//         .replace(/ober/gi, "o")
+//         .replace(/ufat/gi, "u");
+
+// let textoCifrado = texto
+//     .replace(/e/gi, "enter")
+//     .replace(/i/gi, "imes")
+//     .replace(/a/gi, "ai")
+//     .replace(/o/gi, "ober")
+//     .replace(/u/gi, "ufat");
